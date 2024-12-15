a005cf08df4c9007d66a9d5c0eefa50eaa540d4a
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-15 11:46:14 +0300

    Styled using prettier

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,65 +1,74 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   return (
     <section className="w-full h-16 md:h-20 flex items-center justify-between">
-        {/* logo */}
-        <div className="flex items-center gap-4 text-2xl font-bold">
-            <img src="/mangu-logo.jpg" className="w-8 h-8" alt="logo" />
-            {/* This is the space where the nam e of the logo is going to be */}
-            {/* &apos; is a method in javascript that helps it to identify the area as an apostrophe instead of a string */}
-            <span>Mang&apos;u Devs</span>
+      {/* logo */}
+      <div className="flex items-center gap-4 text-2xl font-bold">
+        <img src="/mangu-logo.jpg" className="w-8 h-8" alt="logo" />
+        {/* This is the space where the nam e of the logo is going to be */}
+        {/* &apos; is a method in javascript that helps it to identify the area as an apostrophe instead of a string */}
+        <span>Mang&apos;u Devs</span>
+      </div>
+      {/* Menu mobile */}
+      <div className="md:hidden">
+        <div
+          className="cursor-pointer text-4xl"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? "X" : "☰"}
         </div>
-        {/* Menu mobile */}
-        <div className="md:hidden">
-            <div
-            className="cursor-pointer text-4xl"
-            onClick={()=>setOpen(prev => !prev)}
-            >
-                {open? 'X' : "☰" }
-            </div>
-            {/* mobile link list */}
-            <div className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 bg-[#54c7d6ab] transition-all ease-in-out ${open ? '-right-0' : '-right-[100%]'}`}>
-            <Link to="/">Home</Link>
-            <Link to="/write">Add a new Record</Link>
-            <Link to="/records">Past records view</Link>
-            <Link to="/:slug">Single record</Link>
-            <Link to="/">Note</Link>
-            <SignedOut>
-        <Link to="/login">
-          <button className="py-2 px-4 rounded-3xl bg-blue-500">
-            Login 👍
-          </button>
-        </Link>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-            </div>
+        {/* mobile link list */}
+        <div
+          className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 bg-[#54c7d6ab] transition-all ease-in-out ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <Link to="/">Home</Link>
+          <Link to="/write">Add a new Record</Link>
+          <Link to="/records">Past records view</Link>
+          <Link to="/:slug">Single record</Link>
+          <Link to="/">Note</Link>
+          <SignedOut>
+            <Link to="/login">
+              <button className="py-2 px-4 rounded-3xl bg-blue-500">
+                Login 👍
+              </button>
+            </Link>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </div>
-        {/* Menu Desktop */}
-        <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+      </div>
+      {/* Menu Desktop */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
         <Link to="/">Home</Link>
-            <Link to="/write">Add a new Record</Link>
-            <Link to="/records">Past records view</Link>
-            <Link to="/:slug">Single record</Link>
-            <Link to="/">Note</Link>
-            <SignedOut>
-        <Link to="/login">
-          <button className="py-2 px-4 rounded-3xl bg-blue-500">
-            Login 👍
-          </button>
-        </Link>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-        </div>
+        <Link to="/write">Add a new Record</Link>
+        <Link to="/records">Past records view</Link>
+        <Link to="/:slug">Single record</Link>
+        <Link to="/">Note</Link>
+        <SignedOut>
+          <Link to="/login">
+            <button className="py-2 px-4 rounded-3xl bg-blue-500">
+              Login 👍
+            </button>
+          </Link>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
